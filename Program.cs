de9db6216b08d6e82e1a73aaf2586ba78e64de50
sripushkar100 <42490782+sripushkar100@users.noreply.github.com>
2019-01-07 20:21:10 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,39 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp4
+namespace ConsoleApp3
 {
-    public class Program
+    class Program
     {
-        //Consuming Members of a class from same class of same project
-        private void Test1()
-        {
-            Console.WriteLine("Private Method");
-        }
-        internal void Test2()
-        {
-            Console.WriteLine("Internal Method");
-        }
-        protected void Test3()
-        {
-            Console.WriteLine("Protected Method");
-        }
-        protected internal void Test4()
-        {
-            Console.WriteLine("protected Internal Method");
-        }
-        public void Test5()
-        {
-            Console.WriteLine("Public Method");
-        }
+        int i; bool b;
         static void Main(string[] args)
         {
-            Program p = new Program();
-            p.Test1();
-            p.Test2();
-            p.Test3();
-            p.Test4();
-            p.Test5();
+            Program p = new ConsoleApp3.Program();
+            Console.WriteLine("value of i is:" + p.i);
+            Console.WriteLine("value of b is:" + p.b);
             Console.ReadLine();
         }
     }
