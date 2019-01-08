f4e2b3de0aa3085e79468825ff2d3f1049f144bd
sripushkar100 <42490782+sripushkar100@users.noreply.github.com>
2019-01-08 18:21:57 +0530

    Add files via upload

---
+++ ConsoleApp4/New/Class3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New
+{
+    //Consuming members of a class from non-child class of different projrct
+    class Class3
+    {
+        static void Main()
+        {
+            ConsoleApp4.Program t = new ConsoleApp4.Program();
+            t.Test5();
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp4/New/Demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New
+{
+    class Demo:ConsoleApp4.Program
+    {
+        static void Main(string[] args)
+        {
+            Demo d = new Demo();
+            d.Test3();
+            d.Test4();
+            d.Test5();
+            Console.ReadLine();
+        }
+    }
+}
