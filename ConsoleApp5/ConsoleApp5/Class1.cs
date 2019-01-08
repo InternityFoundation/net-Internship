84cc2fe21531a8fb24b98ddf01e235db34e8b67b
sripushkar100 <42490782+sripushkar100@users.noreply.github.com>
2019-01-08 18:23:15 +0530

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Class1:Mix
+    {
+        public void Test3()
+        {
+            Console.WriteLine("Method 3");
+        }
+        static void Main()
+        {
+            Class1 c = new Class1();
+            c.Test1();
+            c.Test2();
+            c.Test3();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp5/ConsoleApp5/Mix.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+
+{
+    class Mix
+    {
+        public void Test1()
+        {
+            Console.WriteLine("Method 1");
+        }
+        public void Test2()
+        {
+            Console.WriteLine("Method 2");
+        }
+    }
+}
