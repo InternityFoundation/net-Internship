3d967b4d825483d147fe89f21baf756ea724eea4
sripushkar100 <42490782+sripushkar100@users.noreply.github.com>
2019-01-08 18:20:52 +0530

    Add files via upload

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        int i; bool b;
+        static void Main(string[] args)
+        {
+            Program p = new ConsoleApp3.Program();
+            Console.WriteLine("value of i is:" + p.i);
+            Console.WriteLine("value of b is:" + p.b);
+            Console.ReadLine();
+        }
+    }
+}
